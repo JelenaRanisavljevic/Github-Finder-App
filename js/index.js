2d91b4ffda0b3b5eebbb8752eac13aec07c6c515
Jelena Ranisavljevic <ranisavljevicjelenans@gmail.com>
2023-02-03 12:45:38 +0100

    added id's for user information

--- js/index.js
+++ js/index.js
@@ -1,13 +1,6 @@
 function onSearch() {
-    /*to do
-        proveriti da li je korisnik uneo username
-        ako nije izbacicemo mu gresku (alert a mozemo i text ispisati kao poruku u HTML dokumentu)
-        ako je sve okej, mozemo da dobavljamo podatke sa Github API (fetch)
-    */
     console.log('Button clicked');
     if (username.value === '') {
-        //korisnik nije uneo nista u Search
-        //alert('Please Enter Username');
         const errorUsername = document.getElementById('error-username');
         errorUsername.innerHTML = 'Please Enter Username';
     } else {
